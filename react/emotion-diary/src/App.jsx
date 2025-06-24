76c27ec9fd405ee7e51e7b5f790090d9a72e0467
YeJinii <yejindin@gmail.com>
2025-06-24 17:53:11 +0900

    페이지와 공통 컴포넌트 작업

--- react/emotion-diary/src/App.jsx
+++ react/emotion-diary/src/App.jsx
@@ -1,21 +1,28 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { useReducer } from 'react';
+import { 
+    Routes, 
+    Route
+} from 'react-router-dom';
 import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
+import Edit from './pages/Edit';
 import Notfound from './pages/Notfound';
 
+function reducer(state, action){
+    return state;
+}
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 new 페이지
-// 3. "/diary" : 일기를 상세이 조회하는 Diary 페이지
 function App() {
+    const [data, dispatch] = useReducer(reducer, []);
     return (
         <>
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/new" element={<New />}/>
-                <Route path="/diary" element={<Diary />}/>
+                <Route path="/diary/:id" element={<Diary />}/>
+                <Route path="/edit/:id" element={<Edit />}/>
                 <Route path="*" element={<Notfound />}/>
             </Routes>
         </>
